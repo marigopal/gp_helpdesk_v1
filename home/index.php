660a50d7db180a5274699f4d9a5b32443d4f2cc9
unknown <marigopal@hotmail.com>
2021-10-01 13:48:47 +0530

    01102021_Second_Upload

--- home/index.php
+++ home/index.php
@@ -1,14 +1,19 @@
 <?php
-
 session_start();
-echo $_SESSION['accessid'];
-if ($_SESSION['accessid'] == 5) {
+if ($_SESSION['accessid'] == 5) 
+{
     header("Location: /dashboard_it/index");
-} elseif ($_SESSION['accessid'] == 4) {
+} 
+elseif ($_SESSION['accessid'] == 4) 
+{
     header("Location: /dashboard_accounts/index");
-} elseif ($_SESSION['accessid'] == 3) {
+} 
+elseif ($_SESSION['accessid'] == 3) 
+{
     header("Location: /helpdesk_report/index");
-} else {
+} 
+else 
+{
     header("Location: /login/index");
     exit();
 }
